7cd71988f758304d2106d6bb62ae02ca68d55a96
Shouaib Mujawar <75732591+shouaib-mujawar@users.noreply.github.com>
2023-05-03 12:31:34 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,10 +5,10 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "shouaib.mujawar01@gmail.com"
+    username = "something@gmail.com"
     password = os.getenv("pass")
 
-    receiver = "shouaib.mujawar01@gmail.com"
+    receiver = "something@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
